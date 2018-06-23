36e1be558716dde23d33abf445d755b2c99df619
Rohan <nobisro@gmail.com>
2018-06-23 14:38:17 +0800

    created user input, computerSelect, working out kinks with playRound and play()

--- pss.js
+++ pss.js
@@ -1,4 +1,4 @@
-
+/* 
 function computerPlay() {
     let randomNum = Math.ceil(Math.random() * 3)
     if (randomNum == 1 ) {
@@ -54,7 +54,7 @@ function gamePlay(playerSelect, computerSelect) {
 }
 }
 
-// gamePlay(playerSelection, computerPlay());
+gamePlay(playerSelection, computerPlay());
 
 function play() {
     for (let i = 0; i < 5; i++) {
@@ -62,4 +62,103 @@ function play() {
     }
 }
 
-play();
+// play();
+
+// let button = document.querySelector(".button");
+
+// button.onclick = function() {
+//     let name = prompt('What is your name?');
+//     alert('Hello ' + name + ', nice to see you');
+// }
+ */
+
+ function computerPlay() {
+     let rand = Math.round(Math.random() * 2);
+     //creates random number 0 - 2
+
+     if (rand == 0) {
+        return 'paper';
+     } else if (rand == 1) {
+        return 'scissors';
+     } else {
+        return 'stone';
+     }
+ }
+
+let playerPlay = prompt('paper, scissors, stone!');
+playerPlay = playerPlay.toString();
+playerPlay = playerPlay.toLowerCase();
+console.log(playerPlay);
+
+if (playerPlay == 'paper' || playerPlay == 'scissors' || playerPlay == 'stone') {
+    alert('OK!!'); }
+    else {
+    playerPlay = prompt('Try again! paper, scissors, stone!');
+    playerPlay = playerPlay.toString();
+    playerPlay = playerPlay.toLowerCase();
+}
+
+let computerSelect = computerPlay();
+// console.log('computer chooses ' + computerSelect);
+// console.log('player1 chooses ' + playerPlay);
+
+function playRound(playerSelection, computerSelection) {
+
+    let playerPlay = prompt('paper, scissors, stone!');
+    playerPlay = playerPlay.toString();
+    playerPlay = playerPlay.toLowerCase();
+    console.log(playerPlay);
+
+if (playerPlay == 'paper' || playerPlay == 'scissors' || playerPlay == 'stone') {
+    alert('OK!!'); }
+    else {
+    playerPlay = prompt('Try again! paper, scissors, stone!');
+    playerPlay = playerPlay.toString();
+    playerPlay = playerPlay.toLowerCase();
+
+
+    if (playerSelection == 'paper') {
+        if (computerSelection == 'stone') {
+            return 'You Win! Paper beats Stone';
+        } else if (computerSelection == 'paper') {
+            return "It's a tie!!";
+        } else {
+            return 'You Lose! Scissors beats Paper'
+        }
+    }
+
+    if (playerSelection == 'scissors') {
+        if (computerSelection == 'paper') {
+            return 'You Win! Scissors beats paper';
+        } else if (computerSelection == 'scissors') {
+            return "It's a tie!!";
+        } else {
+            return 'You Lose! Stone beats Scissors'
+        }
+    }
+
+    if (playerSelection == 'stone') {
+        if (computerSelection == 'scissors') {
+            return 'You Win! Stone beats Scissors';
+        } else if (computerSelection == 'stone') {
+            return "It's a tie!!";
+        } else {
+            return 'You Lose! Paper beats Stone'
+        }
+    }
+}
+}
+
+// let g1 = playRound(playerPlay, computerSelect);
+// console.log(g1);
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        computerSelect = computerPlay();
+        let newRound = playRound();
+        console.log(newRound);
+    }
+    }
+
+game();
+
